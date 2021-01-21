0d55365af80446c712b4b55ae21079ae04106a33
mstspg1204 <73773317+mstspg1204@users.noreply.github.com>
2021-01-22 00:16:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var helicopterIMG, helicopterSprite
+var packageSprite,packageIMG;
+var packageBody,ground
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+
+	
+
+	
+
+
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+	//Create a Ground
+	
+	 
+	 packageSprite=createSprite(width/2, 80, 10,10);
+	 packageSprite.addImage(packageIMG)
+	 packageSprite.scale=0.2
+
+	 
+
+	box = createSprite(400,650,200,20);
+	box.shapeColor= color("red")
+	box2 = createSprite(300,560,20,200);
+	box2.shapeColor= color("red")
+	box3 = createSprite(500,560,20,200);
+	box3.shapeColor= color("red")
+
+	
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
+	World.add(world, packageBody);
+
+
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	 Matter.Body.setStatic(packageBody,false);
+	 
+    
+  }
+}
+
+
+
